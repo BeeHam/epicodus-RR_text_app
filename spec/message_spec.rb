f50530a0327fd028a164dc36209148d91e133847
brigham thompson & clinton herrick <student@epicodus.com>
2015-11-02 14:20:23 -0800

    adds contacts migration

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe Message, vcr: true do
   it "doesn't save the message if twilio gives an error" do
-    message = Message.new(:body => 'hi', :to =>'1111111', :from => "5035555555")
-    message.save.should be false
+    message = Message.new(:body => 'hi', :to =>'15005550001', :from => "15005550006")
+    message.save
+    expect(message.errors.messages[:base]).to eq ["The 'To' number 15005550001 is not a valid phone number."]
   end
 end
